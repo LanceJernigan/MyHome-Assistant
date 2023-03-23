040620f34b65b55ecce328291ea1092d90d46725
Lance Jernigan <lance.jernigan@claytonhomes.com>
2023-03-23 15:17:32 -0400

    Parse req body

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -36,19 +36,19 @@ interface chatResponse {
 const chatHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   // Inserting a default request body message for testing purposes
   // Can be removed once chat is implemented in front-end
+  const body = JSON.parse(req.body);
+
   await openai
     .createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: req.body.messages
-        ? req.body.messages
-        : [{"role": "user", "content": "Say this is a test!"}],
+      messages: body.messages
+        ? body.messages
+        : [{ role: "user", content: "Say this is a test!" }],
     })
     .then((response) => {
-      console.log(response.data);
       res.status(200).json({ result: response.data });
     })
     .catch((e) => {
-      console.error(`There was an error with ChatGPT: ${e}`);
       res.status(500).send(e);
     });
 };
