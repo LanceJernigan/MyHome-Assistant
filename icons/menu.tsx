84aa6e739bfe03a9b6f84ebba388efd051888f5f
Lance Jernigan <Lance@LanceJernigan.com>
2023-03-23 14:41:09 -0400

    Setup layout (#4)
    * Setup layout
    
    * Fix homes grid
    
    ---------
    
    Co-authored-by: Lance Jernigan <lance.jernigan@claytonhomes.com>

---
+++ icons/menu.tsx
@@ -0,0 +1,34 @@
+export default () => (
+  <svg
+    width="30"
+    height="9"
+    viewBox="0 0 30 9"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="26"
+      cy="4.5"
+      r="4"
+      transform="rotate(90 26 4.5)"
+      fill="black"
+      fillOpacity="0.5"
+    />
+    <circle
+      cx="15"
+      cy="4.5"
+      r="4"
+      transform="rotate(90 15 4.5)"
+      fill="black"
+      fill-opacity="0.5"
+    />
+    <circle
+      cx="4"
+      cy="4.5"
+      r="4"
+      transform="rotate(90 4 4.5)"
+      fill="black"
+      fillOpacity="0.5"
+    />
+  </svg>
+);
